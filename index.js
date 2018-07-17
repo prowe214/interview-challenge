aeff2b44abeb7a697fcf47ff85f8654e9062a42a
Paul Rowe <prowe214@gmail.com>
2018-07-16 20:07:51 -0600

    Allow CORS to react app on another port

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 
 const db = require('./db/db');
 
+// Applying middleware to allow CORS to React App
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    next();
+});
+
 app.get('/data', (req, res) => {
   res.json(JSON.parse(JSON.stringify(db)))
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3001, () => console.log('Example app listening on port 3001!'));
