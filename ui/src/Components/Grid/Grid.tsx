79096edddf28eaaa961965fad66d47f1375d39dd
Paul Rowe <prowe214@gmail.com>
2018-07-16 23:32:18 -0600

    sorts correctly, adds header

--- ui/src/Components/Grid/Grid.tsx
+++ ui/src/Components/Grid/Grid.tsx
@@ -12,10 +12,29 @@ export class Grid extends React.Component<IProps, {}> {
         super(props);
     }
 
+    public sortChart(arr: IRow[]) {
+        return arr.sort( (a, b) => {
+            return a.hourOfDay - b.hourOfDay;
+        })
+    }
+
     public render() {
-        const { data } = this.props;
+        const data = this.sortChart(this.props.data);
+
         return (
             <div className="grid">
+                <div className="row-header">
+                    <div className="col-2">Device</div>
+                    <div className="col-2">Hour</div>
+                    <div className="col-2">Clicks</div>
+                    <div className="col-2">Conversions</div>
+                    <div className="col-2">Avg CPC</div>
+                    <div className="col-2">Avg Position</div>
+                    <div className="col-2">Impressions</div>
+                    <div className="col-2">Max Impressions</div>
+                    <div className="col-2">Search Impression Share</div>
+                    <div className="col-2">Cost</div>
+                </div>
                 {data.map((row, index) => 
                     <GridRow row={row} key={index} />
                 )}
