8496da2f7277be91fad9927d92e9b1d2cd0f6168
Paul Rowe <prowe214@gmail.com>
2018-07-16 23:31:50 -0600

    chart renders lines

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import './App.css';
+import { Chart } from './Components/Chart/Chart';
 import { Grid } from './Components/Grid/Grid';
-import { IState } from './Types/Types';
+import { IColumns, IRow, IState } from './Types/Types';
+import { toTitleCase } from './util/utils';
 
 const dataUrl = 'http://localhost:3001/data';
 
@@ -10,24 +12,61 @@ class App extends React.Component<{}, IState> {
         super(props);
 
         this.state = {
-            data: [],
+            colsData: [],
             isLoaded: false,
+            rowsData: [],
         }
+    }
+
+    /**
+     * getColumns maps all fields and their values to respective hourOfDay so they may be
+     * plotted on a line graph
+     * 
+     * @param rowsArray array of rows returned from data
+     */
+    public getColumns(rowsArray: IRow[]): IColumns[] {
+        // If we wanted to plot all fields...
+        // const allFields = Object.keys(rowsArray[0]);
+        const visibleFields = [
+            'clicks',
+            'conversions',
+            'impressions',
+        ]
+
+        const colsData: IColumns[] = visibleFields.map((field: string) => {
+            const col: IColumns = {
+                data: [],
+                name: toTitleCase(field),
+            };
+            rowsArray.forEach((row: IRow) => {
+                col.data.push(
+                    [
+                        row.hourOfDay,
+                        row[field]
+                    ]
+                )
+            })
+            return col;
+        })
+        console.log(colsData)
+        return colsData;
     }
 
     public componentDidMount() {
         fetch(dataUrl)
             .then( res => res.json() )
             .then( (result) => { 
+                const columns = this.getColumns(result.rows);
                 this.setState({
-                    data: result.rows,
+                    colsData: columns,
                     isLoaded: true,
+                    rowsData: result.rows,
                 }) 
             }, err => console.warn(err) )
     }
 
     public render() {
-        const { data, isLoaded } = this.state;
+        const { colsData, isLoaded, rowsData } = this.state;
 
         if (!isLoaded) {
             return (
@@ -35,8 +74,11 @@ class App extends React.Component<{}, IState> {
             )
         } else {
             return (
-                <Grid data={data} />
-            );
+                <div className="container">
+                    <Chart data={colsData}/>
+                    <Grid data={rowsData} />
+                </div>
+            )
         }
     }
 }
--- ui/src/Components/Chart/Chart.tsx
+++ ui/src/Components/Chart/Chart.tsx
@@ -1,11 +1,56 @@
+import * as Highcharts from 'highcharts';
 import * as React from 'react';
-import { LineChart } from "react-chartkick";
+import { IColumns } from '../../Types/Types';
 
-export class Chart extends React.Component {
+interface IProps {
+    data: IColumns[]
+}
+
+export class Chart extends React.Component<IProps, {}> {
+
+    public renderChart() {
+        const { data } = this.props;
+
+        Highcharts.chart('chart-container', {
+            chart: {
+                type: 'spline'
+            },
+            colors: ['#6CF', '#39F', '#06C', '#036', '#000'],
+            plotOptions: {
+                spline: {
+                    marker: {
+                        enabled: true
+                    }
+                }
+            },
+            series: data,
+            title: {
+                text: 'Ad Campaign Performance On One Day'
+            },
+            xAxis: {
+                title: {
+                    text: 'Hour of Day'
+                },
+                type: 'number'
+            },
+            yAxis: {
+                max: 220,
+                min: 0,
+                title: {
+                    text: 'Value'
+                },
+            },
+        })
+
+    }
+
+    public componentDidMount() {
+        this.renderChart();
+    }
 
     public render() {
         return (
-            
+            <div id="chart-container" />
         )
     }
 }
