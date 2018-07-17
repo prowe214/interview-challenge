cdafffb242079c0c970aa7e28276a990fe45df31
Paul Rowe <prowe214@gmail.com>
2018-07-17 00:00:50 -0600

    abstracted out sortByHour

--- ui/src/util/utils.ts
+++ ui/src/util/utils.ts
@@ -1,3 +1,5 @@
+import { IRow } from './../Types/Types';
+
 export function toTitleCase(str: string) {
     const result = str.replace( /([A-Z])/g, " $1" );
     const finalResult = result.charAt(0).toUpperCase() + result.slice(1);
@@ -5,3 +7,10 @@ export function toTitleCase(str: string) {
     console.log(finalResult);
     return finalResult;
 }
+
+export function sortByHour(arr: IRow[]) {
+    return arr.sort( (a, b) => {
+        return a.hourOfDay - b.hourOfDay;
+    })
+}
+
